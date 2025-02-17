eea4d864c72be5db9277cca1f1b3d9490c3c3009
Vladimir <justlikevova@gmail.com>
2025-02-17 18:55:55 +0300

    get note by id

--- main.py
+++ main.py
@@ -14,3 +14,12 @@ async def get_notes():
         "id": 2, "text": "Вторая заметка",
         "id": 3, "text": "Третья заметка",
         }]}
+
+@app.get("/notes/{note_id}")
+async def get_note_by_id(note_id: int):
+    data = {
+        1: "Первая заметка",
+        2: "Вторая заметка",
+        3: "Третья заметка",
+        }
+    return {"data": data[note_id]}
