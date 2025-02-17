7eadfbe38f41baa66c8b622e7e6de803d1bd192f
Vladimir <justlikevova@gmail.com>
2025-02-17 18:54:29 +0300

    get notes

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/notes/")
+async def get_notes():
+    return {"data": [{
+        "id": 1, "text": "Первая заметка",
+        "id": 2, "text": "Вторая заметка",
+        "id": 3, "text": "Третья заметка",
+        }]}
