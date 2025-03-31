5509998e6361cfe4204ddd076eb6f46f60f35fc2
Vladimir <justlikevova@gmail.com>
2025-03-31 17:34:47 +0300

    fixes

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ async def root():
 
 @app.get("/notes/")
 async def get_notes():
-    return {"data": [{
-        "id": 1, "text": "Первая заметка",
-        "id": 2, "text": "Вторая заметка",
-        "id": 3, "text": "Третья заметка",
-        }]}
+    return {"data": [
+        {"id": 1, "text": "Первая заметка"},
+        {"id": 2, "text": "Вторая заметка"},
+        {"id": 3, "text": "Третья заметка"},
+        ]}
 
 @app.get("/notes/{note_id}")
 async def get_note_by_id(note_id: int):
@@ -22,6 +22,9 @@ async def get_note_by_id(note_id: int):
         2: "Вторая заметка",
         3: "Третья заметка",
         }
+    if note_id not in data.keys():
+        return {}
+    
     return {"data": {
         "id": note_id, "text": data[note_id]
         }}
